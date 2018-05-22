3e86c8f0b737d1395bb5f8726d6830777c36f92a
Trygve Lie <post@trygve-lie.com>
2018-05-22 14:38:21 +0200

    Reworked breaker. Added tests

--- test/breaker.js
+++ test/breaker.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const Breaker = require('../lib/breaker');
+const lolex = require('lolex');
 const tap = require('tap');
 
 /**
@@ -13,15 +14,15 @@ tap.test('Breaker() - object type - should be Breaker', (t) => {
     t.end();
 });
 
-tap.test('Breaker() - no "max" argument set - should set default "max" to 5', (t) => {
+tap.test('Breaker() - no "maxFailures" argument set - should set default "maxFailures" to 5', (t) => {
     const breaker = new Breaker();
-    t.equal(breaker.max, 5);
+    t.equal(breaker.maxFailures, 5);
     t.end();
 });
 
-tap.test('Breaker() - "max" argument set - should set "max" to value', (t) => {
+tap.test('Breaker() - "maxFailures" argument set - should set "maxFailures" to value', (t) => {
     const breaker = new Breaker(10);
-    t.equal(breaker.max, 10);
+    t.equal(breaker.maxFailures, 10);
     t.end();
 });
 
@@ -30,29 +31,99 @@ tap.test('Breaker() - "max" argument set - should set "max" to value', (t) => {
  * .trip()
  */
 
-tap.test('.trip() - breaker is "closed" - should set state to "open" and pending to "false"', (t) => {
+tap.test('.trip() - breaker is "closed" - should count failures', (t) => {
     const breaker = new Breaker();
-    breaker.trip();
+    t.equal(breaker.failures, 0);
 
-    t.equal(breaker.state, 'OPEN');
-    t.false(breaker.pending);
+    breaker.trip();
+    t.equal(breaker.failures, 1);
+
+    breaker.trip();
+    t.equal(breaker.failures, 2);
+
     t.end();
 });
 
-tap.test('.trip() - breaker is "open" - should keep state "open" and pending "false"', (t) => {
+/*
+tap.test('.trip() - breaker is "closed" - reaches max failures - should reset failures', (t) => {
     const breaker = new Breaker();
     breaker.trip();
-
-    t.equal(breaker.state, 'OPEN');
-    t.false(breaker.pending);
-
     breaker.trip();
-
-    t.equal(breaker.state, 'OPEN');
-    t.false(breaker.pending);
+    breaker.trip();
+    breaker.trip();
+    breaker.trip();
+    t.equal(breaker.failures, 0);
 
     t.end();
 });
+*/
+
+
+tap.test('.trip() - breaker is "closed" - reaches max failures - should switch breaker to "open"', (t) => {
+    const breaker = new Breaker();
+    breaker.trip();
+    breaker.trip();
+    breaker.trip();
+    breaker.trip();
+    breaker.trip();
+    t.equal(breaker.state, 'OPEN');
+
+    t.end();
+});
+
+tap.test('.trip() - breaker is "closed" - reaches max failures - should set "tripped" to now + waitTreshold', (t) => {
+    const clock = lolex.install();
+    clock.tick(1000);
+
+    const breaker = new Breaker();
+    breaker.trip();
+    breaker.trip();
+    breaker.trip();
+    breaker.trip();
+    breaker.trip();
+    t.equal(breaker.tripped, 6000);
+
+    clock.uninstall();
+    t.end();
+});
+
+/*
+tap.test('.trip() - breaker is "half_open" - should reset failures', (t) => {
+    const breaker = new Breaker();
+    breaker.state = 'HALF_OPEN';
+
+    breaker.trip();
+    t.equal(breaker.failures, 0);
+
+    t.end();
+});
+
+tap.test('.trip() - breaker is "half_open" - should switch breaker to "open"', (t) => {
+    const breaker = new Breaker();
+    breaker.state = 'HALF_OPEN';
+
+    breaker.trip();
+    t.equal(breaker.state, 'OPEN');
+
+    t.end();
+});
+
+tap.test('.trip() - breaker is "half_open" - should set "tripped" to now + waitTreshold', (t) => {
+    const clock = lolex.install();
+    clock.tick(1000);
+
+    const breaker = new Breaker();
+    breaker.state = 'HALF_OPEN';
+
+    breaker.trip();
+    t.equal(breaker.tripped, 6000);
+
+    clock.uninstall();
+    t.end();
+});
+*/
+
+
 
 
 /**
@@ -67,141 +138,142 @@ tap.test('.check() - No alter to default object  - should return "false"', (t) =
     t.end();
 });
 
-tap.test('.check() - trip and check less then "max" before reset  - should keep state "open"', (t) => {
+tap.test('.check() - trip and check less then "max" before reset  - should return "false" and keep state "closed"', (t) => {
     const breaker = new Breaker();
-    t.equal(breaker.state, 'CLOSED');
-    t.equal(breaker.failures, 0);
 
     let result = breaker.check();
-    t.false(result);
     t.equal(breaker.state, 'CLOSED');
     t.equal(breaker.failures, 0);
+    t.false(result);
 
     breaker.trip();
 
     result = breaker.check();
-    t.equal(breaker.state, 'OPEN');
-    t.true(result);
+    t.equal(breaker.state, 'CLOSED');
+    t.equal(breaker.failures, 1);
+    t.false(result);
+
+    breaker.trip();
 
     result = breaker.check();
-    t.equal(breaker.state, 'OPEN');
-    t.true(result);
+    t.equal(breaker.state, 'CLOSED');
+    t.equal(breaker.failures, 2);
+    t.false(result);
+
+    breaker.trip();
 
     result = breaker.check();
-    t.equal(breaker.state, 'OPEN');
-    t.true(result);
+    t.equal(breaker.state, 'CLOSED');
+    t.equal(breaker.failures, 3);
+    t.false(result);
 
     breaker.reset();
 
     result = breaker.check();
     t.equal(breaker.state, 'CLOSED');
+    t.equal(breaker.failures, 0);
     t.false(result);
 
     t.end();
 });
 
-tap.test('.check() - trip and check less then "max" before reset  - should switch state to "closed"', (t) => {
+tap.test('.check() - trip and check more then "max" before reset  - should return "true" and switch state to "open"', (t) => {
     const breaker = new Breaker();
-    t.equal(breaker.state, 'CLOSED');
-    t.equal(breaker.failures, 0);
 
     let result = breaker.check();
+    t.equal(breaker.state, 'CLOSED');
+    t.equal(breaker.failures, 0);
     t.false(result);
+
+    breaker.trip();
+
+    result = breaker.check();
     t.equal(breaker.state, 'CLOSED');
-    t.equal(breaker.failures, 0);
-
-    breaker.trip();
-
-    result = breaker.check();
-    t.true(result);
-    t.equal(breaker.state, 'OPEN');
     t.equal(breaker.failures, 1);
-
-    breaker.trip();
-
-    result = breaker.check();
-    t.true(result);
-    t.equal(breaker.state, 'OPEN');
-    t.equal(breaker.failures, 2);
-
-    breaker.trip();
-
-    result = breaker.check();
-    t.true(result);
-    t.equal(breaker.state, 'OPEN');
-    t.equal(breaker.failures, 3);
-
-    breaker.reset();
-
-    result = breaker.check();
     t.false(result);
-    t.equal(breaker.state, 'CLOSED');
-    t.equal(breaker.failures, 0);
-
-    t.end();
-});
-
-tap.test('.check() - trip and check more then "max" before reset  - should switch state to "half_open", then to "closed"', (t) => {
-    const breaker = new Breaker();
-    t.equal(breaker.state, 'CLOSED');
-    t.equal(breaker.failures, 0);
-
-    breaker.trip();
-
-    let result = breaker.check();
-    t.true(result);
-    t.equal(breaker.state, 'OPEN');
-    t.equal(breaker.failures, 1);
 
     breaker.trip();
 
     result = breaker.check();
-    t.true(result);
-    t.equal(breaker.state, 'OPEN');
+    t.equal(breaker.state, 'CLOSED');
     t.equal(breaker.failures, 2);
+    t.false(result);
 
     breaker.trip();
 
     result = breaker.check();
-    t.true(result);
-    t.equal(breaker.state, 'OPEN');
+    t.equal(breaker.state, 'CLOSED');
     t.equal(breaker.failures, 3);
+    t.false(result);
 
     breaker.trip();
 
     result = breaker.check();
-    t.true(result);
-    t.equal(breaker.state, 'OPEN');
+    t.equal(breaker.state, 'CLOSED');
     t.equal(breaker.failures, 4);
+    t.false(result);
 
     breaker.trip();
 
     result = breaker.check();
-    t.true(result);
     t.equal(breaker.state, 'OPEN');
     t.equal(breaker.failures, 5);
-
-    breaker.trip();
-
-    result = breaker.check();
-    t.false(result);
-    t.true(breaker.pending);
-    t.equal(breaker.state, 'HALF_OPEN');
-    t.equal(breaker.failures, 0);
-
-    breaker.trip();
-
-    result = breaker.check();
     t.true(result);
+
+    result = breaker.check();
     t.equal(breaker.state, 'OPEN');
-    t.equal(breaker.failures, 1);
+    t.equal(breaker.failures, 5);
+    t.true(result);
 
     breaker.reset();
 
     result = breaker.check();
-    t.false(result);
     t.equal(breaker.state, 'CLOSED');
     t.equal(breaker.failures, 0);
+    t.false(result);
 
+    t.end();
+});
+
+
+tap.test('.check() - "maxTreshold" is reached - should return "false" on first check, "true" on second check', (t) => {
+    const clock = lolex.install();
+    clock.tick();
+
+    const breaker = new Breaker();
+
+    let result = breaker.check();
+    breaker.trip();
+    breaker.trip();
+    breaker.trip();
+    breaker.trip();
+    breaker.trip();
+
+    result = breaker.check();
+    t.equal(breaker.state, 'OPEN');
+    t.equal(breaker.tripped, 5000);
+    t.true(result);
+
+    clock.tick(10000);
+
+    result = breaker.check();
+    t.equal(breaker.state, 'HALF_OPEN');
+    t.equal(breaker.tripped, 5000);
+    t.false(result);
+
+    result = breaker.check();
+    t.equal(breaker.state, 'OPEN');
+    t.equal(breaker.tripped, 15000);
+    t.true(result);
+
+    breaker.reset();
+
+    result = breaker.check();
+    t.equal(breaker.state, 'CLOSED');
+    t.equal(breaker.failures, 0);
+    t.equal(breaker.tripped, -1);
+    t.false(result);
+
+    clock.uninstall();
     t.end();
 });
